0de3c7fa4f984036f45f16ab79d4804d6cc47992
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2022-09-16 15:38:42 -0300

    modelos listo

--- PI-Food-main/api/src/models/Recipe.js
+++ PI-Food-main/api/src/models/Recipe.js
@@ -1,34 +1,34 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
-// Luego le inyectamos la conexion a sequelize.
+// Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        primaryKey: true,
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      image: {
-        type: DataTypes.STRING,
-      },
-      summary: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      score: {
-        type: DataTypes.REAL,
-      },
-      healthScore: {
-        type: DataTypes.REAL,
-      },
-      instructions: {
-        type: DataTypes.TEXT,
-      },
+    id : {
+      type: DataTypes.UUID,        // este tipo de dato es para que no se repita con el de la API 
+      defaultValue: DataTypes.UUIDV4,    
+      allowNull: false,
+      primaryKey : true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    summary:{
+      type : DataTypes.TEXT,
+      allowNull: false
+    },
+    healthScore : {
+      type: DataTypes.STRING
+    },
+    analyzedInstructions:{
+      type : DataTypes.TEXT,
+    },
+    createdInDb:{                                  // esta propiedad la van a tenr solo las comidas que esten en la BD
+      type : DataTypes.BOOLEAN,                    // por lo que es mas facil buscarlas 
+      allowNull: false,
+      defaultValue: true
     }
-  );
+
+  });
 };
---
+++ PI-Food-main/api/src/models/TypeDiet.js
@@ -0,0 +1,17 @@
+const { Sequelize, DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le inyectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('typeDiet',{
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+    }
+  );
+};
