255ef390bffc6aeee39c75022cb28b1f4aac9b26
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2022-11-09 11:45:16 -0300

    deploy

--- PI-Food-main/api/index.js
+++ PI-Food-main/api/index.js
@@ -19,10 +19,19 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const axios = require('axios');
+const dot = require('dotenv');
+dot.config();
+axios.default.baseURL = 'http://localhost:3001';
+var local='';
+if (process.env.PORT == 3001){
+  local='http://localhost:3001';
+}else {
+  local= "https://pi-henry-foods.herokuapp.com";
+}
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log('\x1b[33m%s\x1b[0m', 'server listening at 3001'); // eslint-disable-line no-console
   });
 });
