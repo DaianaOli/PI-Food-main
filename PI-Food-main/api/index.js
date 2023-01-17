411efb23dd63e2854d21c16b0e87972e1348530e
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2023-01-17 12:36:40 -0300

    ...

--- PI-Food-main/api/index.js
+++ PI-Food-main/api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const PORT = process.env.PORT;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`Server listening at ${PORT}`);
   });
