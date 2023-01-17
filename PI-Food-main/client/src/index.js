feef42d8e752b612240842f7929b97414c6f1c4c
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2023-01-16 23:33:50 -0300

    ..

--- PI-Food-main/client/src/index.js
+++ PI-Food-main/client/src/index.js
@@ -10,6 +10,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
+
 ReactDOM.render(
   <Provider store={store}>
      <React.StrictMode>
