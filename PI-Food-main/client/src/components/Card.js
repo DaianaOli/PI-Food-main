0dbfefa34768ca079b54ea5b7ce2010c03f55559
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2023-01-17 12:32:33 -0300

    nuevo intento

--- PI-Food-main/client/src/components/Card.js
+++ PI-Food-main/client/src/components/Card.js
@@ -1,14 +1,30 @@
 import React from "react";
-import  './Card.css'
-export default function Card ({title , img , typeDiets ,  id}) {
-   
-return (
-    <div key = {id} className='card'>
-        <div className='cd'>
+import "./Card.css";
+export default function Card({ title, img, typeDiets, id }) {
+  return (
+    <div key={id} className="card">
+      <div className="cd">
         <h3>{title}</h3>
-        <img  className='cardimg' src = { img? img:'https://img.freepik.com/vector-gratis/vector-ilustracion-dibujos-animados-varias-verduras-sobre-fondo-madera_1441-519.jpg?size=626&ext=jpg&ga=GA1.2.227501000.1662982549' } alt ='img not found' width='100%'/>
-        <div className='tipes'>TypeDiets:<ol>  {typeDiets.map(t =><li key={t.name}> {t.name}</li>)}</ol>  </div>
+        <img
+          className="cardimg"
+          src={
+            img
+              ? img
+              : "https://img.freepik.com/vector-gratis/vector-ilustracion-dibujos-animados-varias-verduras-sobre-fondo-madera_1441-519.jpg?size=626&ext=jpg&ga=GA1.2.227501000.1662982549"
+          }
+          alt="img not found"
+          width="100%"
+        />
+        <div className="tipes">
+          TypeDiets:
+          <ol>
+            {" "}
+            {typeDiets? typeDiets.map((e) =>
+                e.name ? <li key={e.name}>{e.name}</li> : null
+                ) : null}
+          </ol>{" "}
         </div>
+      </div>
     </div>
-)
+  );
 }
--- PI-Food-main/client/src/components/Home.js
+++ PI-Food-main/client/src/components/Home.js
@@ -66,7 +66,7 @@ export default function Home() {
   function handleInputName(e) {
     setSearch(e.target.value);
   }
-  
+
   return (
     <div className="bkg">
       <div className="botones">
